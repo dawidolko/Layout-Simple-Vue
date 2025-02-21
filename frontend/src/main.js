e8e9a9b31d324c95154c00427871e87164215211
d.olko <d.olko@praktyki.ideo.pl>
2025-02-21 10:17:20 +0100

    Deploy build on gh-pages

--- frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "../public/styles/global.css";
-const app = createApp(App);
-app.mount("#app");
--- frontend/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Instalacja zaleznosci..."
-npm install || { echo "Wystapil blad podczas instalacji zaleznosci."; exit 1; }
-echo "Uruchamianie projektu..."
-npm start
